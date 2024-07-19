97fa810a5f74f71bfb794c8d13dbd3096ff8a3a7
NewGen2022 <amirgimari@gmail.com>
2024-07-19 20:57:25 +0300

    page that displays all messages

--- frontend/src/pages/MessageBoard.jsx
+++ frontend/src/pages/MessageBoard.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const MessageBoard = () => {
-    return <div></div>;
+    const [messages, setMessages] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:1919/')
+            .then((res) => {
+                setMessages(res.data);
+                setLoading(false);
+            })
+            .catch((err) => {
+                console.error(err);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            {messages.map((msg) => (
+                <div key={msg._id}>
+                    {msg.username}: {msg.text}
+                </div>
+            ))}
+            <Link to="/new">
+                <button>Add message</button>
+            </Link>
+        </div>
+    );
 };
 
 export default MessageBoard;
