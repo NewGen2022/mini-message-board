14183fd77b57de610e1ab7cc7c0aea883115c0e1
NewGen2022 <amirgimari@gmail.com>
2024-07-19 20:55:09 +0300

    changed port and set server auto opening

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: 1919,
+        port: 8877,
         open: true,
     },
 });
