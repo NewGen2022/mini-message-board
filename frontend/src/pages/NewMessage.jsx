6713dfb757015f6c4599c6c42a3d51d5df9c1231
NewGen2022 <amirgimari@gmail.com>
2024-07-19 20:57:51 +0300

    page to add new message

--- frontend/src/pages/NewMessage.jsx
+++ frontend/src/pages/NewMessage.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const NewMessage = () => {
-    return <div></div>;
+    const navigate = useNavigate();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData.entries());
+
+        try {
+            await axios.post('http://localhost:1919/new', data);
+            navigate('/');
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username
+                <input type="text" name="username" required />
+            </label>
+            <label>
+                Message
+                <input type="text" name="text" required />
+            </label>
+            <button type="submit">Add message</button>
+        </form>
+    );
 };
 
 export default NewMessage;
