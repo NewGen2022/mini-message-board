eddc8e6daa124c7cd951a8a6be1c31900b47cf10
NewGen2022 <amirgimari@gmail.com>
2024-07-21 13:57:11 +0300

    optimized code

--- frontend/src/pages/NewMessage.jsx
+++ frontend/src/pages/NewMessage.jsx
@@ -1,21 +1,31 @@
 import '../styles/NewMessage.css';
 import axios from 'axios';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const NewMessage = () => {
     const navigate = useNavigate();
+    const API_URL = 'http://localhost:1919/new';
+    const [formData, setFormData] = useState({ username: '', text: '' });
+    const [isLoading, setIsLoading] = useState(false);
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({ ...prevData, [name]: value }));
+    };
 
-        const formData = new FormData(event.target);
-        const data = Object.fromEntries(formData.entries());
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setIsLoading(true);
 
         try {
-            await axios.post('http://localhost:1919/new', data);
+            await axios.post(API_URL, formData);
             navigate('/');
         } catch (error) {
+            alert(`Error with connection: ${error.message}`);
             console.error('Error:', error);
+        } finally {
+            setIsLoading(false);
         }
     };
 
@@ -25,15 +35,30 @@ const NewMessage = () => {
             <div id="inputs">
                 <label>
                     Username
-                    <input type="text" name="username" required />
+                    <input
+                        type="text"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange}
+                        required
+                        autoComplete="username"
+                        disabled={isLoading}
+                    />
                 </label>
                 <label>
                     Message
-                    <textarea type="text" name="text" required />
+                    <textarea
+                        name="text"
+                        value={formData.text}
+                        onChange={handleChange}
+                        required
+                        autoComplete="off"
+                        disabled={isLoading}
+                    />
                 </label>
             </div>
-            <button type="submit" id="add-msg-btn">
-                Add message
+            <button type="submit" id="add-msg-btn" disabled={isLoading}>
+                {isLoading ? 'Adding...' : 'Add message'}
             </button>
         </form>
     );
